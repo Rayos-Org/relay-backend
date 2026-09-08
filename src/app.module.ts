b63142be9d7c3783b87035ebde470d674c3c0e0d
thisisouvik <souvikmandals10@gmail.com>
2026-09-08 21:27:28 +0530

    chore: register modules and add wallet-sdk

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { DatabaseModule } from '@common/database/database.module';
 import { RedisModule } from '@common/redis/redis.module';
 import { WebAuthnModule } from '@modules/webauthn/webauthn.module';
 import { RelayModule } from '@modules/relay/relay.module';
+import { SessionsModule } from '@modules/sessions/sessions.module';
+import { IndexerModule } from '@modules/indexer/indexer.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, RedisModule, WebAuthnModule, RelayModule],
+  imports: [ConfigModule, DatabaseModule, RedisModule, WebAuthnModule, RelayModule, SessionsModule, IndexerModule],
   controllers: [],
   providers: [],
 })
